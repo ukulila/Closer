35f88a68c5a042330f9b2c9eac2ab85a1b4d175a
Gales <k.gales@rubika-edu.com>
2019-04-10 10:01:35 +0200

    MobileShortcut

---
+++ Assets/Scripts/MobileShortcuts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobileShortcuts : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+ }
