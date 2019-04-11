e867b1f9d940c1151a1843c4794c322236916788
Lila <l.grimaldi@rubika-edu.com>
2019-04-11 17:47:25 +0200

    Mise en place (scène Menu)  et script caméra Menu

---
+++ Assets/Scripts/MainMenuCamControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+
+public class MainMenuCamControl : MonoBehaviour
+{
+    public CinemachineVirtualCamera MainMenu;
+    public CinemachineVirtualCamera Options;
+    public CinemachineVirtualCamera SelectionNiveaux;
+    public CinemachineVirtualCamera Timeline;
+
+    public List<CinemachineVirtualCamera> cameras;
+    public CinemachineVirtualCamera CamToDeactivate;
+
+    private bool mainMenu;
+    private bool options;
+    private bool Selection;
+    private bool timeLine;
+
+
+    private void Awake()
+    {
+        cameras = new List<CinemachineVirtualCamera>() { MainMenu, Options, SelectionNiveaux, Timeline };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuActif();
+        
+        for (int i = 0; i < cameras.Count - 1; i++)
+        {
+            if (cameras[i].gameObject.activeInHierarchy)
+            {
+                CamToDeactivate = cameras[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MainMenu.gameObject.SetActive(true);
+
+        /*void ActivationOptions()
+        {
+            // ce qu'il se passe dans le menu
+
+        }*/
+    }
+
+    void MainMenuActif()
+    {
+        
+
+
+    }
+
+    public void SwitchCam(CinemachineVirtualCamera CamToActivate)
+    {
+        
+        CamToDeactivate.gameObject.SetActive(false);
+        CamToActivate.gameObject.SetActive(true);
+
+        CamToDeactivate = CamToActivate;
+    }
+}
