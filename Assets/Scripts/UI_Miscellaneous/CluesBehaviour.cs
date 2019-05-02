aeebf2af3410df4460094631722b3f1d79613d6b
Gales <k.gales@rubika-edu.com>
2019-05-02 12:01:38 +0200

    ProtoClues (Discarded)
    K

---
+++ Assets/Scripts/UI_Miscellaneous/CluesBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CluesBehaviour : MonoBehaviour
+{
+    public List<bool> clues;
+    public Transform cluesBoard;
+    public Material material;
+    public float amount;
+    public bool pop;
+
+
+    public void Update()
+    {
+
+        
+        for (int i = 0; i < clues.Count; i++)
+        {
+            
+
+            if (clues[i])
+            {
+                material = transform.GetChild(i + 1).GetComponent<Renderer>().material;
+                amount = material.GetFloat("_Amount");
+                material.SetFloat("_Amount", amount);
+
+             //   Debug.Log(transform.GetChild(i + 1));
+
+                pop = true;
+            }
+          /*  else
+            {
+                amount = 1;/*
+                if (amount >= 0 && amount <= 1)
+                {
+
+                    amount += 0.01f;
+
+                }
+            }*/
+        }
+
+
+        if(pop)
+        {
+            if (amount >=0)
+            {
+                amount -= 0.005f;
+                material.SetFloat("_Amount", amount);
+
+            }
+            /*else
+            {
+                pop = false;
+            }*/
+        }
+
+        
+    }
+
+   
+}
---
+++ Assets/Scripts/UI_Miscellaneous/PopUpCanvas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpCanvas : MonoBehaviour
+{
+    public bool isToggled;
+    public Vector3 targetPos;
+    public Vector3 offPos;
+    public GameObject clue;
+    [Range(0.01f, 1)]
+    public float speed;
+
+    
+    public void Update()
+    {
+        if (!isToggled)
+        {
+            clue.transform.localPosition = Vector3.Lerp(transform.localPosition, offPos, speed);            
+        }
+
+        if (isToggled)
+        {
+            clue.transform.localPosition = Vector3.Lerp(transform.localPosition, targetPos, speed);
+        }
+
+
+        if( clue.transform.localPosition == offPos)
+        {
+            clue.SetActive(false);
+
+        }
+
+    }
+
+
+    public void PopUp()
+    {
+        if (!isToggled)
+        {
+            clue.SetActive(true);
+            isToggled = true;
+            return;
+        }
+        else if (isToggled)
+        {
+            isToggled = false;
+            return;
+
+        }
+
+
+    }
+
+
+
+
+}
