68969d954d936e5c40d3171a867b870ed398c0c4
François Noël <f.noel@rubika-edu.com>
2019-05-07 19:22:12 +0200

    Lila this is for you

---
+++ Assets/Scripts/SelectionMenu/LevelCloseUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelCloseUp : MonoBehaviour
+{
+    public Image polaroideImage;
+    public TextMeshProUGUI levelNameCU;
+    public TextMeshProUGUI levelDescriptionCU;
+    public TextMeshProUGUI clueNameCU;
+    public TextMeshProUGUI clueDescriptionCU;
+
+    public static LevelCloseUp Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
