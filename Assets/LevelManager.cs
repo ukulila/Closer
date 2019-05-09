de78bc09903e94163ae68c9e550afce2c0e8a0b5
Lila <l.grimaldi@rubika-edu.com>
2019-05-09 12:26:35 +0200

    Mise en place Animation  Selection Niveaux

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour 
+{
+    public List<LevelSumUp> levels;
+    public int progressionIndex;
+
+    public void SetUpProgression()
+    {
+        for (int i = 0; i < progressionIndex; i++)
+        {
+
+        }
+    }
+}
