5d5580c86eb55aa89e82a6967deb5b449230e0af
Gales <k.gales@rubika-edu.com>
2019-05-23 13:44:21 +0200

    Menu WiP02

--- Assets/TimeLineScript.cs
+++ Assets/TimeLineScript.cs
@@ -8,7 +8,7 @@ using UnityEngine.EventSystems;
 
 public class TimeLineScript : MonoBehaviour
 {
-    
+
     public GraphicRaycaster m_Raycaster;
     public PointerEventData m_PointerEventData;
     public EventSystem m_EventSystem;
@@ -18,11 +18,23 @@ public class TimeLineScript : MonoBehaviour
     public bool polaroidInteraction;
     private Vector3 transPos;
     public Vector3 offset;
-    public Transform Target;
+    public List<Transform> TargetRangement;
+    public List<Transform> TargetClues;
+
     public Vector3 unTouchedOffset;
     public AnimationCurve LerpCurve;
     public float LerpSpeed;
     public Vector3 originPos;
+
+    public List<GameObject> Deduction;
+    public List<GameObject> Rangement;
+    public List<GameObject> truth;
+
+    public List<GameObject> reality;
+
+    [Space]
+
+    public GameObject submitButton;
 
 
     void Start()
@@ -32,23 +44,23 @@ public class TimeLineScript : MonoBehaviour
 
     void Update()
     {
-     
-        if(Input.GetMouseButtonUp(0) && polaroidInteraction)
-        {
 
+        if (Input.GetMouseButtonUp(0) && polaroidInteraction)
+        {
+            Cursor.visible = true;
             currentPolaroid.position = originPos;
             polaroidInteraction = false;
 
         }
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             m_PointerEventData = new PointerEventData(m_EventSystem);
             m_PointerEventData.position = Input.mousePosition;
 
             List<RaycastResult> results = new List<RaycastResult>();
 
-            if(m_Raycaster!= null)
+            if (m_Raycaster != null)
             {
                 m_Raycaster.Raycast(m_PointerEventData, results);
 
@@ -56,11 +68,11 @@ public class TimeLineScript : MonoBehaviour
 
             foreach (RaycastResult result in results)
             {
-                if(result.gameObject.GetComponent<ThisIsPolaroid>() != null)
+                if (result.gameObject.GetComponent<ThisIsPolaroid>() != null && result.gameObject.GetComponent<ThisIsPolaroid>().isUnlocked == true)
                 {
                     Cursor.visible = false;
 
-                    
+
 
                     currentPolaroid = result.gameObject.transform;
                     originPos = currentPolaroid.position;
@@ -79,26 +91,77 @@ public class TimeLineScript : MonoBehaviour
 
         }
 
-        if(currentPolaroid != null)
+        if (currentPolaroid != null)
         {
-
-            if(currentPolaroid.position.x - Target.position.x < 180 && currentPolaroid.position.x - Target.position.x > -70 && currentPolaroid.position.y - Target.position.y > -70 && currentPolaroid.position.y - Target.position.y < 70)
+            for (int i = 0; i < TargetRangement.Count; i++)
             {
-
-                if(Target.childCount == 0 )
+                if (currentPolaroid.position.x - TargetRangement[i].position.x < 180 && currentPolaroid.position.x - TargetRangement[i].position.x > -70 && currentPolaroid.position.y - TargetRangement[i].position.y > -70 && currentPolaroid.position.y - TargetRangement[i].position.y < 70)
                 {
-                    LerpSpeed = LerpCurve.Evaluate(currentPolaroid.position.x - Target.position.x);
 
-                    polaroidInteraction = false;
+                    if (TargetRangement[i].childCount == 0)
+                    {
+                        LerpSpeed = LerpCurve.Evaluate(Time.deltaTime);
 
-                    currentPolaroid.SetParent(Target);
-                    currentPolaroid.position = Vector3.Lerp(currentPolaroid.position, Target.position + unTouchedOffset, LerpSpeed);
-                    Cursor.visible = true;
+                        polaroidInteraction = false;
+                        Deduction.Remove(currentPolaroid.gameObject);
+
+                        isInListRangement();
+
+                        if (!isInListRangement())
+                        {
+                            Rangement.Add(currentPolaroid.gameObject);
+                        }
+
+                        currentPolaroid.SetParent(TargetRangement[i]);
+                        currentPolaroid.position = TargetRangement[i].position + unTouchedOffset;
+                        Cursor.visible = true;
+                    }
+
+
                 }
-                
 
+                if (currentPolaroid.position.x - TargetClues[i].position.x < 180 && currentPolaroid.position.x - TargetClues[i].position.x > -70 && currentPolaroid.position.y - TargetClues[i].position.y > -70 && currentPolaroid.position.y - TargetClues[i].position.y < 70)
+                {
+
+                    if (TargetClues[i].childCount == 0)
+                    {
+                        LerpSpeed = LerpCurve.Evaluate(Time.deltaTime);
+
+                        polaroidInteraction = false;
+
+                        isInListDeduction();
+
+                        if(!isInListDeduction())
+                        {
+                            Deduction.Add(currentPolaroid.gameObject);
+                        }
+
+
+
+
+                        Rangement.Remove(currentPolaroid.gameObject);
+                        currentPolaroid.SetParent(TargetClues[i]);
+                        currentPolaroid.position = TargetClues[i].position + unTouchedOffset;
+                        Cursor.visible = true;
+                    }
+
+
+                }
             }
+
         }
+
+
+        if (Deduction.Count >= 4)
+        {
+            submitButton.SetActive(true);
+        }
+        else
+        {
+            submitButton.SetActive(false);
+
+        }
+
 
 
     }
@@ -109,4 +172,58 @@ public class TimeLineScript : MonoBehaviour
 
         currentPolaroid.position = transPos;
     }
+    public void submitCheck()
+    {
+        truth.Clear();
+
+        for (int i = 0; i < TargetClues.Count; i++)
+        {
+
+            truth.Add(TargetClues[i].GetChild(0).gameObject);
+
+        }
+
+        bool CheckMatch()
+        {
+            if (truth.Count != reality.Count)
+                return false;
+            for (int u = 0; u < truth.Count; u++)
+            {
+                if (truth[u] != reality[u])
+                    return false;
+            }
+            return true;
+        }
+
+        if (CheckMatch() == true)
+        {
+            Debug.Log("win");
+        }
+
+    }
+
+
+
+    bool isInListRangement()
+    {
+        for (int y = 0; y < Rangement.Count; y++)
+        {
+            if (currentPolaroid.gameObject == Rangement[y])
+                return true;
+        }
+        return false;
+    }
+
+    bool isInListDeduction()
+    {
+        for (int y = 0; y < Deduction.Count; y++)
+        {
+            if (currentPolaroid.gameObject == Deduction[y])
+                return true;
+        }
+        return false;
+    }
+
+
+
 }
