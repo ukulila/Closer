c68cc006f98664bc2e468429a53e009e70f9fd59
François Noël <f.noel@rubika-edu.com>
2019-05-27 19:36:51 +0200

    Dialogue Tweaking / Trail Renderer / Camera Dolly

---
+++ Assets/helpForCircle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class helpForCircle : MonoBehaviour
+{
+
+    public int vertexCount = 40;
+    public float lineWidth = 0.2f;
+    public float radius;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        SetupCircle();
+    }
+
+    private void SetupCircle()
+    {
+        lineRenderer.widthMultiplier = lineWidth;
+
+        float deltaTheta = (2f * Mathf.PI) / vertexCount;
+        float theta = 0f;
+
+        lineRenderer.positionCount = vertexCount;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            Vector3 pos = new Vector3(radius * Mathf.Cos(theta), 0f, radius * Mathf.Sin(theta));
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+}
