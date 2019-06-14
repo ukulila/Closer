540f7e6af7ec649226ab5d8fb96e25414416828e
Gales <k.gales@rubika-edu.com>
2019-06-14 02:30:10 +0200

    Menu  -Polaroid Actualised

---
+++ Assets/ParticleRandomMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRandomMenu : MonoBehaviour
+{
+
+    public List<GameObject> particules;
+    public List<AudioClip> ambiance;
+    public int randomInt;
+
+    public void OnEnable()
+    {
+        SelectRandomAmbiance();
+    }
+
+    public void SelectRandomAmbiance()
+    {
+        randomInt = Random.Range(0, particules.Count);
+        particules[randomInt].SetActive(true);
+
+        for (int i = 0; i < particules.Count; i++)
+        {
+            if (i != randomInt)
+                particules[i].SetActive(false);
+        }
+    }
+}
